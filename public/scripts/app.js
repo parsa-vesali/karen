595406cafdedb935476e677f631ff815b6c449d0
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-12-19 08:58:22 +0330

    the project updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 const toggleThemeBtns = document.querySelectorAll('.toggle-theme');
-
+const searchBtnOpen = document.querySelector('.search-btn-open')
+const searchModal = document.querySelector('.search-modal')
+const searchModalClose = document.querySelector('.search-modal-close')
 
 // Function to toggle theme
 const toggleTheme = () => {
@@ -14,4 +16,17 @@ const toggleTheme = () => {
 
 toggleThemeBtns.forEach(btn => {
     btn.addEventListener('click', toggleTheme);
+});
+
+searchBtnOpen.addEventListener('click', () => {
+    searchModal.classList.add('active')
+})
+searchModalClose.addEventListener('click', () => {
+    searchModal.classList.remove('active')
+})
+
+document.addEventListener('click', (event) => {
+    if (searchModal.classList.contains('active') && !searchModal.contains(event.target) && !searchBtnOpen.contains(event.target)) {
+        searchModal.classList.remove('active');
+    }
 });
